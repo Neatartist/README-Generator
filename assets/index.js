cfa907792edfd66db24b97e4a80686ac6232225f
Daniel Van Kirk <davk0810@gmail.com>
2023-07-24 15:55:04 -0400

    index update04

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require(inquirer)
 const fs = require('fs')
-const generateMarkdown = require('./utils/generateMarkdown.js')
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
 const questions = [
   {
@@ -51,14 +51,14 @@ const questions = [
     if (User_usage) {
       return true;
     } else {
-      console.log('please describe the usage of the project.');
+      console.log('Please describe the usage of the project.');
       return false;
     }
   }
   },
   {
     type: 'input',
-    message: 'what were your contributions to this project?',
+    message: 'What were your contributions to this project?',
     name: 'contributions',
     validate: user_contribution => {
       if (user_contribution){
@@ -71,8 +71,8 @@ const questions = [
   },
   {
     type: 'input',
-    message: 'Please enter your github username',
-    name:'github'
+    message: 'Please enter your Github username',
+    name:'github',
     validate: user_github => {
       if (user_github){
         return true;
@@ -84,28 +84,59 @@ const questions = [
   },
   {
     type: 'checkbox',
-    message: 'choose the license you want',
+    message: 'Choose the license you want',
     name: 'license',
-    validate: user_license => {
-      if (user_license){
-        return true;
-      } else {
-        console.log('Please choose a valid license');
-        return false;
-      }
+    choices: [
+      'Mozilla Public License',
+      'Apache License',
+      'MIT License',
+      'Boost Software License',
+      'The Unlicense',
+      'GNU AGPLv3',
+    ],
+  validate: user_license => {
+    if (user_license){
+      return true;
+    } else {
+      console.log('Please choose a valid license');
+      return false;
     }
+  }
   },
   {
     type: 'input',
-    message: 
+    message: 'What is your plan to test the project?',
+    name: 'test',
+    validate: user_test => {
+      if (user_test){
+        return true;
+      } else {
+        console.log('Please explain how you plan to test the project');
+        return false; 
+      }
+    }
+  },
 ];
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('sucess! your README file has been summoned into existense!');
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions) 
+  .then (function (userInput) {
+      console.log(userInput)
+      writeToFile("README.md", generateMarkdown(userInput);
+  });
+}
 
 // Function call to initialize app
 init();
